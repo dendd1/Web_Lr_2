1057690e42efa86692ce7f7a119a1971fe41cd1b
SusanWay <zaev_2030@mail.ru>
2021-10-04 12:53:25 +0300

    Новый

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+            //Функция показа
+            function show_reg(state)
+            {
+            document.getElementById('window_reg').style.display = state;  
+            document.getElementById('gray').style.display = state;    
+            } 
+      
+      
+      
+      //Функция показа
+      function show_auth(state)
+      {
+      document.getElementById('window_auth').style.display = state;  
+      document.getElementById('gray').style.display = state;    
+      } 
