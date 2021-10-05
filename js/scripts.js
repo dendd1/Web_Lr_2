7112e513a2d59a2f9e64c519cea7b584e3a5a72b
SusanWay <zaev_2030@mail.ru>
2021-10-06 02:02:53 +0300

    Микро фиксы

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,38 @@
-            //Функция показа
-            function show_reg(state)
-            {
-            document.getElementById('window_reg').style.display = state;  
-            document.getElementById('gray').style.display = state;    
-            } 
-      
-      
-      
-      //Функция показа
-      function show_auth(state)
-      {
-      document.getElementById('window_auth').style.display = state;  
-      document.getElementById('gray').style.display = state;    
-      } 
+var catalog_btn_1 = document.getElementById('catalog_btn_1');
+var catalog_btn_2 = document.getElementById('catalog_btn_2');
+var catalog_btn_3 = document.getElementById('catalog_btn_3');
+var catalog_btn_4 = document.getElementById('catalog_btn_4');
+
+catalog_btn_1.onclick = change_catalog_active_btn_1;
+catalog_btn_2.onclick = change_catalog_active_btn_2;
+catalog_btn_3.onclick = change_catalog_active_btn_3;
+catalog_btn_4.onclick = change_catalog_active_btn_4;
+
+function change_catalog_active_btn_1()
+{
+      document.getElementById('catalog_btn_1').classList.add('catalog_btn_active');
+      document.getElementById('catalog_btn_2').classList.remove('catalog_btn_active');
+      document.getElementById('catalog_btn_3').classList.remove('catalog_btn_active');
+      document.getElementById('catalog_btn_4').classList.remove('catalog_btn_active');
+}
+function change_catalog_active_btn_2()
+{
+      document.getElementById('catalog_btn_1').classList.remove('catalog_btn_active');
+      document.getElementById('catalog_btn_2').classList.add('catalog_btn_active');
+      document.getElementById('catalog_btn_3').classList.remove('catalog_btn_active');
+      document.getElementById('catalog_btn_4').classList.remove('catalog_btn_active');
+}
+function change_catalog_active_btn_3()
+{
+      document.getElementById('catalog_btn_1').classList.remove('catalog_btn_active');
+      document.getElementById('catalog_btn_2').classList.remove('catalog_btn_active');
+      document.getElementById('catalog_btn_3').classList.add('catalog_btn_active');
+      document.getElementById('catalog_btn_4').classList.remove('catalog_btn_active');
+}
+function change_catalog_active_btn_4()
+{
+      document.getElementById('catalog_btn_1').classList.remove('catalog_btn_active');
+      document.getElementById('catalog_btn_2').classList.remove('catalog_btn_active');
+      document.getElementById('catalog_btn_3').classList.remove('catalog_btn_active');
+      document.getElementById('catalog_btn_4').classList.add('catalog_btn_active');
+}          
